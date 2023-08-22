b0f8f20562e9183869eabc40b2630e702e40c85e
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-22 18:11:40 -0300

    feat: init sqlite

--- source/index.php
+++ source/index.php
@@ -6,12 +6,24 @@
 // para ser utilizado como um "database fake" no arquivo registros.php.
 
 // Exemplo de conexão com o SQLite usando PDO (referência: https://www.php.net/manual/pt_BR/ref.pdo-sqlite.php)
+try {
+    $pdo = new PDO(
+        'sqlite:../data/db.sqlite',
+        '',
+        '',
+        [
+            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+        ]
+    );
+    
+    $query = "SELECT * FROM registros";
+    $stmt = $pdo->query($query);
 
-$pdo = new PDO(
-    'sqlite:../data/db.sq3',
-    '',
-    '',
-    [
-        \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-    ]
-);
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        echo '<pre>';
+        print_r($row);
+        echo '</pre>';
+    }
+} catch (\Throwable $th) {
+    echo 'Erro na conexão: ' . $e->getMessage();
+}
