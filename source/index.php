a4921029afbce472788e36077ec05bda994b091d
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-23 18:36:38 -0300

    feat: start class routes and controller

--- source/index.php
+++ source/index.php
@@ -1,29 +1,17 @@
 <?php
+require_once './api/db/database.php';
+require_once './api/http/request.php';
+require_once './api/http/response.php';
+require_once './api/http/router.php';
+require_once './api/controllers/record.php';
 
-// Nesse arquivo constam alguns exemplos. Caso use um framework, sinta-se livre para substituir esse arquivo.
 
-// Caso não consiga utilizar o SQLite (esperamos que consiga), vamos disponibilizar um array com os dados
-// para ser utilizado como um "database fake" no arquivo registros.php.
+date_default_timezone_set('America/Sao_Paulo');
+define('URL', 'http://localhost:8000/api');
 
-// Exemplo de conexão com o SQLite usando PDO (referência: https://www.php.net/manual/pt_BR/ref.pdo-sqlite.php)
-try {
-    $pdo = new PDO(
-        'sqlite:../data/db.sqlite',
-        '',
-        '',
-        [
-            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-        ]
-    );
-    
-    $query = "SELECT * FROM registros";
-    $stmt = $pdo->query($query);
+$request = new Request();
+$objRouter = new Router(URL, $request);
 
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        echo '<pre>';
-        print_r($row);
-        echo '</pre>';
-    }
-} catch (\Throwable $th) {
-    echo 'Erro na conexão: ' . $e->getMessage();
-}
+include __DIR__.'/api/routes/record.php';
+
+$objRouter->run()->sendResponse();
