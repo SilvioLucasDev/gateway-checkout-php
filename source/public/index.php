8f7ed08df15b1f3696cdd87169d093c50e0d2192
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-24 20:31:33 -0300

    refactor: add importe do arquivo de config no index

--- source/public/index.php
+++ source/public/index.php
@@ -1,15 +1,9 @@
 <?php
 
+require __DIR__ . '/../config/config.php';
 require __DIR__ . '/../vendor/autoload.php';
 
 use App\http\config\Router;
-
-date_default_timezone_set('America/Sao_Paulo');
-define('DSN', 'sqlite:../../data/db.sqlite');
-define('USERNAME', '');
-define('PASSWORD', '');
-
-define('URL', 'http://localhost:8000/api');
 
 $objRouter = new Router(URL);
 
