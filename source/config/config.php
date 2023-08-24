02fc27d2c8976c6bc5b6d58bf5174a409e39b0db
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-24 20:29:24 -0300

    feat: add file config

---
+++ source/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+date_default_timezone_set('America/Sao_Paulo');
+
+define('DSN', 'sqlite:../../data/db.sqlite');
+define('USERNAME', '');
+define('PASSWORD', '');
+
+define('URL', 'http://localhost:8000/api');
