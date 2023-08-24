10d755c9cfd21b422b7380a91beb89b7904506c9
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-23 21:37:02 -0300

    refactor: move logic of consult to repository

--- source/app/controllers/record.php
+++ source/app/controllers/record.php
@@ -1,25 +1,14 @@
 <?php
-  require_once './app/http/request.php';
-  class RecordController {
+require_once './app/http/request.php';
+require_once './app/repository/record.php';
 
-    public static function index(Request $request) {
-      $queryParams = $request->getQueryParams();
-      $deleted = isset($queryParams['deleted']) && $queryParams['deleted'] !== '' ? $queryParams['deleted'] : '';
-      $type = isset($queryParams['type']) && $queryParams['type'] !== '' ? $queryParams['type'] : '';
+class RecordController
+{
 
-      if($deleted !== '') { $conditions[] = 'deleted = :deleted'; }
-      if($type !== '') { $conditions[] = 'type = :type'; }
-
-      $sql = 'SELECT * FROM registros';
-      if (!empty($conditions)) { $sql .= ' WHERE ' . implode(' AND ', $conditions); }
-
-      $db = Database::connect();
-      $stmt = $db->prepare($sql);
-
-      if($deleted !== '') { $stmt->bindParam(':deleted', $deleted); }
-      if($type !== '') { $stmt->bindParam(':type', $type); }
-
-      $stmt->execute();
-      return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
+  public static function index(Request $request)
+  {
+    $queryParams = $request->getQueryParams();
+    $repository = new RecordRepository();
+    return $repository->get($queryParams);
   }
+}
