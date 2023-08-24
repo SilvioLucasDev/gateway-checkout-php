a588890975374222780522a3c3a82a1968026ad9
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-24 20:50:25 -0300

    refactor: apply RecordControllerFactory

--- source/routes/record.php
+++ source/routes/record.php
@@ -1,54 +1,39 @@
 <?php
 
+use App\factories\RecordControllerFactory;
 use App\http\config\Response;
-use App\http\controllers\RecordController;
-use App\infra\repositories\sqlite\RecordRepository;
-
 
 $objRouter->get('/registros', [
   function ($request) {
-    $repository = new RecordRepository();
-    $controller = new RecordController($repository);
-    return new Response($controller->index($request));
-  }
-]);
-
-$objRouter->get('/registros', [
-  function ($request) {
-    $repository = new RecordRepository();
-    $controller = new RecordController($repository);
+    $controller = RecordControllerFactory::create();
     return  new Response($controller->index($request));
   }
 ]);
 
 $objRouter->get('/registro/{id}', [
   function ($id) {
-    $repository = new RecordRepository();
-    $controller = new RecordController($repository);
+    $controller = RecordControllerFactory::create();
     return  new Response($controller->show($id));
   }
 ]);
 
 $objRouter->post('/registro', [
   function ($request) {
-    $repository = new RecordRepository();
-    $controller = new RecordController($repository);
+    $controller = RecordControllerFactory::create();
     return  new Response($controller->store($request));
   }
 ]);
 
 $objRouter->delete('/registro/{id}', [
   function ($id) {
-    $repository = new RecordRepository();
-    $controller = new RecordController($repository);
+    $controller = RecordControllerFactory::create();
     return  new Response($controller->destroy($id));
   }
 ]);
 
 $objRouter->put('/registro/{id}', [
   function ($id, $request) {
-    $repository = new RecordRepository();
-    $controller = new RecordController($repository);
+    $controller = RecordControllerFactory::create();
     return  new Response($controller->update($id, $request));
   }
 ]);
