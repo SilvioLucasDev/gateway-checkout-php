88c3a140c288a1b649e85b5b701487b72bb253a8
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-25 12:23:01 -0300

    feat: add FieldValidation

---
+++ source/app/http/validations/FieldValidation.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Validations;
+
+use App\Http\Exceptions\ValidationException;
+
+class FieldValidation
+{
+
+  public static function isRequired(array $data, array $requiredFields): void
+  {
+    foreach ($requiredFields as $field) {
+      if (!isset($data[$field])) throw new ValidationException("The field $field is required!");
+    }
+  }
+
+  public static function isNumber(array $data, array $requiredFields): void
+  {
+    foreach ($requiredFields as $field) {
+      if (isset($data[$field])) {
+        if (!is_numeric($data[$field])) throw new ValidationException("The $field field must be a number!");
+      }
+    }
+  }
+
+  public static function isString(array $data, array $requiredFields): void
+  {
+    foreach ($requiredFields as $field) {
+      if (isset($data[$field])) {
+        if (!is_string($data[$field])) throw new ValidationException("The $field field must be a string!");
+      }
+    }
+  }
+
+  public static function isBool(array $data, array $requiredFields): void
+  {
+    foreach ($requiredFields as $field) {
+      if (isset($data[$field])) {
+        if (!is_bool($data[$field]) && $data[$field] !== 0 && $data[$field] !== 1 && $data[$field] !== '0' && $data[$field] !== '1') throw new ValidationException("The $field field must be a boolean!");
+      }
+    }
+  }
+}
